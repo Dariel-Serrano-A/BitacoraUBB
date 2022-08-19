dc629a45963b31959e5f58635596bd2a6a5b0d5b
Dariel <dariel.serrano1601@alumnos.ubiobio.cl>
2022-08-19 00:40:36 -0400

    Correccion de estilos csss bootswatch

--- client.new/src/app/app.module.ts
+++ client.new/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BitacoraListComponent } from './components/bitacora-list/bitacora-list.
 import { BitacorasService } from './services/bitacoras.service';
 import { BitacoraDetailComponent } from './components/bitacora-detail/bitacora-detail.component';
 
+
 //Parte Shauffa 
 
 import { CalendarioModule } from './components/calendario/calendario.module';
